65c8c09675fb21b259ae8600c26e171b4bd6e76c
lam <a253143598@qq.com>
2018-08-27 15:38:51 +0800

    hint: Updates were rejected because the tag already exists in the remote.

--- LMUtilsDemo/LMUtilsDemo/LMUtils/Foundation/NSNotificationCenter+LMUtils.m
+++ LMUtilsDemo/LMUtilsDemo/LMUtils/Foundation/NSNotificationCenter+LMUtils.m
@@ -13,9 +13,6 @@
 + (void)postNotificationOnMainThread:(NSNotificationName)name
                               object:(id)anObject
                             userInfo:(NSDictionary *)userinfo {
-    
-    
-    
     if (pthread_main_np()) {
         [[self defaultCenter] postNotificationName:name object:anObject userInfo:userinfo];
     } else {
