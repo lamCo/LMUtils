327775003710ae61892a3de623cd5cb51a1e7777
lam <a253143598@qq.com>
2018-11-02 18:07:22 +0800

    添加功能

--- LMUtilsDemo/LMUtilsDemo/LMUtils/Kit/UIImage+LMUtils.h
+++ LMUtilsDemo/LMUtilsDemo/LMUtils/Kit/UIImage+LMUtils.h
@@ -22,13 +22,34 @@
   颜色转image
  @param color 颜色
  */
-+ (UIImage *)imageWithColor:(UIColor *)color ;
++ (UIImage *)imageWithColor:(UIColor *)color;
 
 /**
  颜色转image
  @param color 颜色
  */
 +(UIImage *)imageWithColor:(UIColor *)color size:(CGSize)size;
+
+
+/**
+ 创建二维码 图片
+ 
+ @param content 内容
+ @param size 宽高
+ @return image
+ */
++ (UIImage *)creatCIQRCodeImage:(NSString *)content imageSize:(CGFloat)size;
+
+/**
+ *
+ 根据CIImage生成指定大小的UIImage
+ *
+ * @param image CIImage
+ * @param size 图片宽度
+ *
+ * @return 生成高清的UIImage
+ */
++ (UIImage *)creatNonInterpolatedUIImageFormCIImage:(CIImage *)image withSize:(CGFloat)size;
 
 
 
--- LMUtilsDemo/LMUtilsDemo/LMUtils/Kit/UIImage+LMUtils.m
+++ LMUtilsDemo/LMUtilsDemo/LMUtils/Kit/UIImage+LMUtils.m
@@ -75,4 +75,48 @@
 }
 
 
+/// 创建二维码 图片
++ (UIImage *)creatCIQRCodeImage:(NSString *)content imageSize:(CGFloat)size {
+    // 1.创建过滤器，这里的@"CIQRCodeGenerator"是固定的
+    CIFilter *filter = [CIFilter filterWithName:@"CIQRCodeGenerator"];
+    // 2.恢复默认设置
+    [filter setDefaults];
+    
+    // 3. 给过滤器添加数据
+    NSString *dataString = content;
+    NSData *data = [dataString dataUsingEncoding:NSUTF8StringEncoding];
+    // 注意，这里的value必须是NSData类型
+    [filter setValue:data forKeyPath:@"inputMessage"];
+    // 4. 生成二维码
+    CIImage *outputImage = [filter outputImage];
+    return [self creatNonInterpolatedUIImageFormCIImage:outputImage withSize:size];
+}
+
+
+/// 根据CIImage生成指定大小的UIImage
++ (UIImage *)creatNonInterpolatedUIImageFormCIImage:(CIImage *)image withSize:(CGFloat)size {
+    @autoreleasepool {
+        CGRect extent = CGRectIntegral(image.extent);
+        CGFloat scale = MIN(size/CGRectGetWidth(extent), size/CGRectGetHeight(extent));
+        
+        // 1. 创建bitmap
+        size_t width = CGRectGetWidth(extent) * scale;
+        size_t height = CGRectGetHeight(extent) * scale;
+        CGColorSpaceRef cs = CGColorSpaceCreateDeviceGray();
+        CGContextRef bitmapRef = CGBitmapContextCreate(nil, width, height, 8, 0, cs, (CGBitmapInfo)kCGImageAlphaNone);
+        CIContext *context = [CIContext contextWithOptions:nil];
+        CGImageRef bitmapImage = [context createCGImage:image fromRect:extent];
+        CGContextSetInterpolationQuality(bitmapRef, kCGInterpolationNone);
+        CGContextScaleCTM(bitmapRef, scale, scale);
+        CGContextDrawImage(bitmapRef, extent, bitmapImage);
+        
+        // 2.保存bitmap图片
+        CGImageRef scaledImage = CGBitmapContextCreateImage(bitmapRef);
+        CGContextRelease(bitmapRef);
+        CGImageRelease(bitmapImage);
+        return [UIImage imageWithCGImage:scaledImage];
+    }
+}
+
+
 @end
